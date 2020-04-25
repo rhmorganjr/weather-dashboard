57b73ec3d6b942d2d14ab45f2d823039f4e28c79
Russell Morgan <rhmorgan13@comcast.net>
2020-04-25 12:29:28 -0500

    syncpoint

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -12,7 +12,7 @@ function listItemHandler(e) {
   e.preventDefault();
 
   console.log(e.target.attributes.value);
-  let loc = document.getElementById('city');
+  let loc = document.getElementById('searchCity');
   loc.value = e.target.attributes.value.value;
 }
 
@@ -29,8 +29,42 @@ function loadWeatherStorage() {
   }
 }
 
-function searchHandler() {
+function searchHandler(e) {
+  e.preventDefault();
   console.log("search");
+  let city = document.getElementById('searchCity').value;
+console.log("sH city="+city);
+console.log('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=38502b70b289d62a156e29aa933997f8');
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' 
+    + city 
+    + ',us&APPID=38502b70b289d62a156e29aa933997f8&units=imperial')
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response);
+    console.log("id="+response.id);
+    console.log("name="+response.name);
+    console.log("feels="+response.main.feels_like);
+    console.log("temp = "+response.main.temp);
+
+    let detailCity = document.getElementById('detailCity');
+    detailCity.textContent = response.name;
+
+    let currentDateTime = document.getElementById('currentDateTime');
+    let tempDate = moment();//response.dt;
+    console.log("tempDate="+tempDate);
+    currentDateTime.textContent = tempDate.format('MM/DD/YYYY');
+
+    let currentTemp = document.getElementById('currentTemp');
+    currentTemp.textContent = "Current Temp: " + Math.round(response.main.temp) + " F";
+
+    let humidity = document.getElementById('humidity');
+    humidity.textContent = "Humidity: " + response.main.humidity + "%";
+
+    let windSpeed = document.getElementById('windSpeed');
+    windSpeed.textContent = "Wind Speed: " + response.wind.speed + " MPH";
+  });
 }
 
 document.getElementById("search").addEventListener('click', searchHandler);
