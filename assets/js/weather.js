c549295b76001e26b1bddad3b3dfcfd531574a72
Russell Morgan <rhmorgan13@comcast.net>
2020-04-26 15:25:47 -0500

    changed api from forecast to oneCall

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,12 +1,6 @@
 let cityList = document.getElementById('city-list');
-//http://openweathermap.org/img/wn/10d@2x.png
+let cityDateImg = document.getElementById('cityDateImg');
 
-function initializeStorage() {
-  let primer = [{'city':'Austin', 'cityId':'', }, {'city':'Anaheim', 'cityId':''}];
-  localStorage.setItem("weatherDashboard", JSON.stringify(primer));
-}
-
-//initializeStorage();
 
 function listItemHandler(e) {
   e.preventDefault();
@@ -51,42 +45,70 @@ console.log('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPI
     console.log("icon="+response.weather[0].icon);
     console.log("lat, lon="+response.coord.lat+", "+response.coord.lon);
 
-    let detailCity = document.getElementById('detailCity');
-    //let currentDateTime = document.getElementById('currentDateTime');
     let tempDate = moment();
-    detailCity.textContent = response.name + "  (" + tempDate.format('MM/DD/YYYY') + ")  ";
-    let cityDateImg = document.querySelector('.city-date-img');
+    cityDateImg.textContent = response.name + "  (" + tempDate.format('MM/DD/YYYY') + ")  ";
+    
     let imgEl = document.createElement('img');
     imgEl.setAttribute('src', 'http://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png');
     imgEl.setAttribute('height', 32);
     imgEl.setAttribute('width', 32);
     cityDateImg.appendChild(imgEl);
-    //currentDateTime.textContent = tempDate.format('MM/DD/YYYY');
+  
+    //fetch('https://api.openweathermap.org/data/2.5/onecall?lat='
+    //+ response.coord.lat + '&lon=' + response.coord.lon + '&appid=38502b70b289d62a156e29aa933997f8&units=imperial');
 
+    return fetch('https://api.openweathermap.org/data/2.5/onecall?lat='
+    + response.coord.lat + '&lon=' + response.coord.lon 
+    + '&appid=38502b70b289d62a156e29aa933997f8&units=imperial');
+    
+  })
+  .then((oneCallResponse) => {
+    return oneCallResponse.json();
+  })
+  .then (oneCallResponse => {
+    console.log("1callR="+oneCallResponse);
     let currentTemp = document.getElementById('currentTemp');
-    currentTemp.textContent = "Current Temp: " + Math.round(response.main.temp) + " F";
+    currentTemp.textContent = "Current Temp: " + Math.round(oneCallResponse.current.temp) + " F";
 
     let humidity = document.getElementById('humidity');
-    humidity.textContent = "Humidity: " + response.main.humidity + "%";
+    humidity.textContent = "Humidity: " + oneCallResponse.current.humidity + "%";
 
     let windSpeed = document.getElementById('windSpeed');
-    windSpeed.textContent = "Wind Speed: " + response.wind.speed + " MPH";
+    windSpeed.textContent = "Wind Speed: " + oneCallResponse.current.wind_speed + " MPH";
 
-    const uvFetch = 'https://api.openweathermap.org/data/2.5/uvi/forecast?appid=38502b70b289d62a156e29aa933997f8' +
-    '&lat=' + response.coord.lat + '&lon=' + response.coord.lon;
-    console.log("uvF="+uvFetch);
-    return fetch(uvFetch);
-  })
-  .then((uvResponse) => {
-    return uvResponse.json();
-  })
-  .then (uvResponse => {
-    console.log("uv val="+uvResponse[0].value);
+    console.log("oneCall: "+oneCallResponse);
+    console.log("uv val="+oneCallResponse.current.uvi);
     let uv =document.getElementById('uv');
-    uv.textContent = "UV Index: " + uvResponse[0].value;
+    uv.textContent = "UV Index: " + oneCallResponse.current.uvi;
+
+
+    console.log("pressure="+oneCallResponse.daily[0].pressure);
+
+    console.log("dt="+oneCallResponse.current.dt);
+    let date = new Date(oneCallResponse.daily[0].dt * 1000);
+    console.log((new Date(oneCallResponse.daily[1].dt * 1000)).toLocaleString());
+    console.log((new Date(oneCallResponse.daily[2].dt * 1000)).toLocaleString());
+    console.log((new Date(oneCallResponse.daily[3].dt * 1000)).toLocaleString());
+    console.log((new Date(oneCallResponse.daily[4].dt * 1000)).toLocaleString());
+    console.log((new Date(oneCallResponse.daily[5].dt * 1000)).toLocaleString());
   });
 }
+
+
 
 document.getElementById("search").addEventListener('click', searchHandler);
 
 loadWeatherStorage();
+
+
+function initializeStorage() {
+  let primer = [{'city':'Austin', 'lastSearch':''},
+            {'city':'Anaheim', 'lastSearch':''},
+            {'city':'San Antonio', 'lastSearch':''},
+            {'city':'Paris', 'lastSearch':''}
+  ];
+  localStorage.setItem("weatherDashboard", JSON.stringify(primer));
+}
+
+//initializeStorage();
+
